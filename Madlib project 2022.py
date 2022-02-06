5eb6704b93470fa4995c5c8521bb784a1e237d5b
laurenerose1 <67874014+laurenerose1@users.noreply.github.com>
2022-02-05 20:08:03 -0800

    Add files via upload

---
+++ Madlib project 2022.py
@@ -0,0 +1,18 @@
+##create a mad lib using the following poem
+## create 4 variables for user inputs
+color = input("Pick a color:")
+plural_noun = input("Pick a plural noun:")
+flavor = input ("Pick a food flavor:")
+persons_name = input("Pick a random person famous or not: ")
+yes_or_no = input ("Pick yes or no")
+
+##print out madlib with responses
+
+print("Roses are", color)
+print( plural_noun, "are blue")
+print("Sugar is", flavor)
+print("I love", persons_name)
+print("We asked if", persons_name,"would be your valentine?")
+print(" and They said", yes_or_no)
+print("Thanks for playing my madlib game!")
+
